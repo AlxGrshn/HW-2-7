d2de3217bee3903861d32199bd411b162daacb7f
Frysed <frysed@gmail.com>
2022-10-01 19:54:44 +0300

    add full logic

--- HW-2-6/LoginViewController.swift
+++ HW-2-6/LoginViewController.swift
@@ -12,26 +12,38 @@ class LoginViewController: UIViewController {
     @IBOutlet var userNameTF: UITextField!
     @IBOutlet var passwordTF: UITextField!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
-
-    @IBAction func logInButtonDidTapped() {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
+        welcomeVC.userName = userNameTF.text
+    }
 
+    @IBAction func logInButtonDidTapped() {
+        let title = "Invalid login or password"
+        let message = "Please, enter correct login and password"
+        if userNameTF.text != "Teacher" && passwordTF.text != "Swift" {
+            showAlert(with: title, and: message)
+        }
+    }
+    
     @IBAction func forgotUserNameButtonDidTapped() {
         let title = "Ooops!"
         let message = "Your name is Teacher 👨‍🏫"
         showAlert(with: title, and: message)
     }
     
-    
     @IBAction func forgotPasswordButtonDidTapped() {
         let title = "Ooops!"
         let message = "Your password is Swift 🧐"
         showAlert(with: title, and: message)
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        userNameTF.text = ""
+        passwordTF.text = ""
     }
     
     private func showAlert(with title: String, and message: String)  {
--- HW-2-6/WelcomeViewController.swift
+++ HW-2-6/WelcomeViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
 
+    @IBOutlet var userNameLabel: UILabel!
+    
+    var userName: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        userNameLabel.text = userName
     }
-
-
 }
