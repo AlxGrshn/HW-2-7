deccafeda8912e86e97494caa221adb08716b52a
Frysed <frysed@gmail.com>
2022-10-01 16:01:31 +0300

    add base design

---
+++ HW-2-6/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  HW-2-6
+//
+//  Created by Alexander Grishin on 01.10.2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
