564a0692bfc723c8214252d85206c9cf59f22906
Frysed <frysed@gmail.com>
2022-10-01 16:54:36 +0300

    add two alertViews for forgot login and password buttons

--- HW-2-6/LoginViewController.swift
+++ HW-2-6/LoginViewController.swift
@@ -9,9 +9,34 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
 
+    @IBAction func logInButtonDidTapped() {
+    }
+    
+
+    @IBAction func forgotUserNameButtonDidTapped() {
+        let title = "Ooops!"
+        let message = "Your name is Teacher 👨‍🏫"
+        showAlert(with: title, and: message)
+    }
+    
+    
+    @IBAction func forgotPasswordButtonDidTapped() {
+        let title = "Ooops!"
+        let message = "Your password is Swift 🧐"
+        showAlert(with: title, and: message)
+    }
+    
+    private func showAlert(with title: String, and message: String)  {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
 }
